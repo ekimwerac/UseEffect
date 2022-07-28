ff0f3655b182bb666a5421d24c06dd599657b754
Mike Carew <mike@carew.uk>
2022-07-28 15:49:17 +0200

    Infinite Loop first checking

---
+++ src/EffectsDemoInfiniteLoop.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function EffectsDemoInfiniteLoop() {
+    const [title, setTitle] = useState("default title");
+    const titleRef = useRef();
+    useEffect(() => {
+      console.log("useEffect title");
+      document.title = title;
+    });
+    useEffect(() => {
+      console.log("useEffect local storage");
+      const persistedTitle = localStorage.getItem("title");
+      setTitle(persistedTitle || []);
+    });
+    console.log("render");
+    const handleClick = () => setTitle(titleRef.current.value);
+    return (
+      <div>
+        <input ref={titleRef} />
+        <button onClick={handleClick}>change title</button>
+      </div>
+    );
+  }
+
+  export default EffectsDemoInfiniteLoop;
+  
