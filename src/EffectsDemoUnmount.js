d066fb09f642e825ea9958be7ff7e9e4bab467af
Mike Carew <mike@carew.uk>
2022-07-29 16:18:30 +0200

    dependency meddling

--- src/EffectsDemoUnmount.js
+++ src/EffectsDemoUnmount.js
@@ -2,14 +2,21 @@ import React, {useEffect, useState} from "react";
 // import Counter from "./Counter";
 
 function Counter() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-      const interval = setInterval(function () {
-        setCount((prev) => prev + 1);
-      }, 1000);
-    }, []);
-    return <p>and the counter counts {count}</p>;
-  }
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("useEffect")
+    const interval = setInterval(function () {
+      setCount((prev) => prev + 1);
+    }, 1000);
+    // return optional function for cleanup
+    // in this case acts like componentWillUnmount
+    return () => {
+      console.log("cleanup");
+      clearInterval(interval);
+    }
+  }, [count]);
+  return <p>and the counter counts {count}</p>;
+}
 
 function EffectsDemoUnmount() {
     const [unmount, setUnmount] = useState(false);
