be58f8252d918c97ad9da1931f4de57be4d79da7
Mike Carew <mike@carew.uk>
2022-07-29 14:51:53 +0200

    added files from stash

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+// import React, {useEffect, useState} from "react";
+
+// function Counter() {
+//     const [count, setCount] = useState(0);
+//     useEffect(() => {
+//       const interval = setInterval(function () {
+//         setCount((prev) => prev + 1);
+//       }, 1000);
+//     }, []);
+//     return <p>and the counter counts {count}</p>;
+//   }
+  
+//   export default Counter;
